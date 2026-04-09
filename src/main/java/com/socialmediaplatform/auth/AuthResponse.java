f61a880252a78490c5365a7cf3beb8b7879f07e8
Haji Shaikh <hajisk89999@gmail.com>
2026-04-09 20:04:33 +0530

    Added authentication module (register, login, DTO, service)

---
+++ src/main/java/com/socialmediaplatform/auth/AuthResponse.java
@@ -0,0 +1,33 @@
+package com.socialmediaplatform.auth;
+
+public class AuthResponse {
+
+    private String message;
+    private String username;
+
+    // Constructors
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String message, String username) {
+        this.message = message;
+        this.username = username;
+    }
+
+    // Getter & Setter
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
